412985b8c7e530b057e64fc68b77bcd099f6f79f
Oleg Kireev <okireev@ubic.tech>
2022-10-17 18:20:22 +0400

    @/auth/auth.module: import UserModule, JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.SECRET_JWT_KEY,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
 })
 export class AuthModule {}
