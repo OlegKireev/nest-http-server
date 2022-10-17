4bfe13bc37bf4a8434cfb6a7104f8136d9d9c6a9
Oleg Kireev <okireev@ubic.tech>
2022-10-17 12:29:53 +0400

    @/users/users.controller: describe methods in swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,23 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UsersService } from './users.service';
+import { UserModel } from './users.model';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Пользователи')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Создание нового пользователя' })
+  @ApiResponse({ status: 200, type: UserModel })
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
   }
 
+  @ApiOperation({ summary: 'Получение всех пользователей' })
+  @ApiResponse({ status: 200, type: [UserModel] })
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
