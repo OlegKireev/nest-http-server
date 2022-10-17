321f4c9f04773751fb17b35b1b431b4d81ca6240
Oleg Kireev <okireev@ubic.tech>
2022-10-17 15:26:28 +0400

    @/{users, roles}: import roleService to userService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,14 @@ import { UserModel } from './users.model';
 import { UsersService } from './users.service';
 import { RoleModel } from 'roles/roles.model';
 import { UserRoleModel } from 'user-roles/user-roles.model';
+import { RolesModule } from 'roles/roles.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([UserModel, RoleModel, UserRoleModel])],
+  imports: [
+    SequelizeModule.forFeature([UserModel, RoleModel, UserRoleModel]),
+    RolesModule,
+  ],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { RolesService } from 'roles/roles.service';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UserModel } from './users.model';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectModel(UserModel) private userRepository: typeof UserModel,
+    @InjectModel(UserModel)
+    private userRepository: typeof UserModel,
+    private roleService: RolesService,
   ) {}
 
   async createUser(dto: CreateUserDto) {
