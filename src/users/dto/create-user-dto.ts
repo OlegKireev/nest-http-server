cbac9b53b467a8ebd7423004cdf0a91514c71f68
Oleg Kireev <okireev@ubic.tech>
2022-10-17 12:30:23 +0400

    @/users/dto/create-user-dto: describe properties in swagger

--- src/users/dto/create-user-dto.ts
+++ src/users/dto/create-user-dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'user@yandex.ru',
+    description: 'Почта пользователя',
+  })
   readonly email: string;
+  @ApiProperty({
+    example: 'user@yandex.ru',
+    description: 'Незашифрованный пароль пользователя',
+  })
   readonly password: string;
 }
