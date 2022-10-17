353352e7a9e6dee85790dad745120b6d31c59761
Oleg Kireev <okireev@ubic.tech>
2022-10-17 15:11:16 +0400

    @/roles/roles/service: create createRole, getRoleByValue services

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateRoleDto } from './dto/create-role-dto';
+import { RoleModel } from './roles.model';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+  constructor(
+    @InjectModel(RoleModel) private rolesRepository: typeof RoleModel,
+  ) {}
+
+  async createRole(dto: CreateRoleDto) {
+    const upperCasedValue = {
+      ...dto,
+      value: dto.value.toUpperCase(),
+    };
+    const role = await this.rolesRepository.create(upperCasedValue);
+    return role;
+  }
+
+  async getRoleByValue(value: string) {
+    const upperCassedValue = value.toUpperCase();
+    const role = await this.rolesRepository.findOne({
+      where: { value: upperCassedValue },
+    });
+    return role;
+  }
+}
