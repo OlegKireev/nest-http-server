84ab97b6a53c66f60c9629e92016dc7afb26cecd
Oleg Kireev <okireev@ubic.tech>
2022-10-17 12:28:44 +0400

    @/main: create swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT || 5001;
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest-course')
+    .setDescription('Докуметация rest api')
+    .setVersion('0.1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, swaggerDocument);
 
   await app.listen(port, () => {
     console.log(`Server has been started on http://localhost:${port}`);
