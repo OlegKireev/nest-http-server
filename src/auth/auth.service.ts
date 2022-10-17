117ff792438b9aa2d98cb600a21b3bc7cb369709
Oleg Kireev <okireev@ubic.tech>
2022-10-17 19:09:47 +0400

    @/auth/auth.service: implement registration logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'user/dto/create-user-dto';
+import { UserModel } from 'user/user.model';
+import { UserService } from 'user/user.service';
 import { JwtService } from '@nestjs/jwt';
-
+import { hash } from 'bcryptjs';
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(dto: CreateUserDto) {
     console.log('');
   }
 
-  async registration(dto: CreateUserDto) {}
+  async registration(dto: CreateUserDto) {
+    const candidate = await this.userService.getUserByEmail(dto.email);
+
+    if (candidate) {
+      throw new HttpException(
+        'Пользователь с тамим e-mail уже зарегистрирован',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const hashedPassword = await hash(dto.password, 5);
+
+    const user = await this.userService.createUser({
+      ...dto,
+      password: hashedPassword,
+    });
+    return this.generateToken(user);
+  }
+
+  async generateToken(user: UserModel) {
+    const payload = {
+      email: user.email,
+      id: user.id,
+      roles: user.roles,
+    };
+
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
 }
